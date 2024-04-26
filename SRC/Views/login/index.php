bfa890963a5e4a8be110eb35eec9ef192731be89
Rodrigo <vegetassj4@msn.com>
2024-04-26 09:41:42 -0300

    ajute página home

--- SRC/Views/login/index.php
+++ SRC/Views/login/index.php
@@ -1,32 +1,30 @@
 <?php require_once __DIR__ . "../../topo.php" ?>
 <div class="container">
-        <div class="bg-body-tertiary rounded-3 row">
-            <div class="col divisao_bottom form-control-padronizado" id="modLogin">
-                <h3>Login</h3>
-                <form method="post" id="formLogin" action="">
-                    <div class="form-group">
-                        <label class="col-form-label" for="nip">Nip: </label>
-                        <input class="form-control form-control-sm form-control-padronizado" type="text" name="nip"
-                            id="nip">
+    <div class="bg-body-tertiary rounded-3 row">
+        <div class="col divisao_bottom form-control-padronizado" id="modLogin">
+            <h3>Login</h3>
+            <form method="post" id="formLogin" action="">
+                <div class="form-group">
+                    <label class="col-form-label" for="nip">Nip: </label>
+                    <input class="form-control form-control-sm form-control-padronizado" type="text" name="nip" id="nip">
+                </div>
+                <div class="form-group">
+                    <label class="col-form-label" for="senha">Senha: </label>
+                    <input class="form-control form-control-sm form-control-padronizado" type="password" name="senha" id="senha">
+                </div>
+                <br>
+                <div class="form-group row">
+                    <div class="col-sm-3">
+                        <input type="button" id="btnLogin" value="Login" class="btn btn-primary">
                     </div>
-                    <div class="form-group">
-                        <label class="col-form-label" for="senha">Senha: </label>
-                        <input class="form-control form-control-sm form-control-padronizado" type="password"
-                            name="senha" id="senha">
-                    </div>
-                    <br>
-                    <div class="form-group row">
-                        <div class="col-sm-3">
-                            <input type="button" id="btnLogin" value="Login" class="btn btn-primary">
-                        </div>
-                    </div>
-                </form>
-                <span class="alerta"></span>
-            </div>
+                </div>
+            </form>
+            <span class="alerta"></span>
         </div>
     </div>
+</div>
 </body>
+
 </html>
 
-<script src="../../scripts/jquery.js"></script>
-<script src="../../scripts/scripts.js"></script>
+<?php require_once __DIR__ . "../../rodape.php" ?>
