c150053a4d29bda427cb9e26c9c079d1125d0668
Rodrigo <vegetassj4@msn.com>
2024-07-02 15:22:04 -0300

    retirada do hash do identificador

--- SRC/Services/DocumentoServices.php
+++ SRC/Services/DocumentoServices.php
@@ -380,7 +380,7 @@ class DocumentoServices extends SistemaServices
 
         $descTipoDocumento = $TipoDocumentoRepository->BuscarTipoDocumentoID($tags->tipoDoc);
 
-        return $funcoes->tratarStringUTF8("Identificador: " . hash('sha256', $tags->identificador) . "; Classe: " . $tags->classe . "; Data de Produção: " . $tags->dataProdDoc . "; Destinação: " . $tags->destinacaoDoc . "; Genero: " . $tags->genero . "; PrazoGuarda: " . $tags->prazoGuarda . ";Tipo Documental: " . $descTipoDocumento . "; Responsavel Digitalização: " . $tags->respDigitalizacao . "; Observação: " . $tags->observacao);
+        return $funcoes->tratarStringUTF8("Identificador: " . $tags->identificador . "; Classe: " . $tags->classe . "; Data de Produção: " . $tags->dataProdDoc . "; Destinação: " . $tags->destinacaoDoc . "; Genero: " . $tags->genero . "; PrazoGuarda: " . $tags->prazoGuarda . ";Tipo Documental: " . $descTipoDocumento . "; Responsavel Digitalização: " . $tags->respDigitalizacao . "; Observação: " . $tags->observacao);
     }
     public function abrirArquivo(string $caminhoarquivo, string $cifrado): string
     {
@@ -534,7 +534,7 @@ class DocumentoServices extends SistemaServices
                 'assunto' => $tag['assunto'],
                 'Autor' => $tag['autor'],
                 'DataDigitalizacao' => new DateTime(),
-                'IdentDocDigital' => hash('sha256', $tag['identificador']),
+                'IdentDocDigital' => $tag['identificador'],
                 'RespDigitalizacao' => $tag['respDigitalizacao'],
                 'Titulo' => $tag['titulo'],
                 'TipoDocumento' =>  $documentos['tipoDocumento'],
